bf947842e45abd70a8175522412fdd757e8069ba
jlirascand <jlirascand1@educacion.navarra.es>
2025-11-12 20:06:15 +0100

    12/11/2025

--- Assets/Scripts/CatAutodelete.cs
+++ Assets/Scripts/CatAutodelete.cs
@@ -2,21 +2,17 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour
 {
-    // Tiempo en segundos antes de eliminarse
     private float timeToSelfDestruct = 5f;
 
     void Start()
     {
-        // Iniciar la coroutine para eliminar el objeto
         StartCoroutine(DestroyAfterTime());
     }
 
     private System.Collections.IEnumerator DestroyAfterTime()
     {
-        // Esperar 5 segundos
         yield return new WaitForSeconds(timeToSelfDestruct);
-        
-        // Eliminar el objeto
+
         Destroy(gameObject);
     }
 }
